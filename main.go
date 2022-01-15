312e0e90b690afd5b24fb9df8d58561e383de5ab
kakeru Aoyama <kakeru0520sou1@gmail.com>
2022-01-15 11:25:50 +0900

    前日と1週間前の増加数を表示

--- main.go
+++ main.go
@@ -6,7 +6,9 @@ import (
 	"log"
 	"net/http"
 	"net/url"
+	"strconv"
 	"strings"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
@@ -41,12 +43,15 @@ func main() {
 		ctx.Header("Content-Type", "text/html; charset=UTF-8")
 
 		cumulative_value := fetchApi(number_date, encoded_prefecture)
+		back_1day := day_back(-1, date)
+		back_1day_cumulative_value := fetchApi(back_1day, encoded_prefecture)
+		difference_1days := subtract_strings(cumulative_value[0].Npatients, back_1day_cumulative_value[0].Npatients)
 
-		// t, _ := time.Parse(number_date, "20181220")
-		// t2 := t.AddDate(0, 0, -1)
-		// t3 := t2.Format("20060102")
+		back_7day := day_back(-7, date)
+		back_7day_cumulative_value := fetchApi(back_7day, encoded_prefecture)
+		difference_7days := subtract_strings(cumulative_value[0].Npatients, back_7day_cumulative_value[0].Npatients)
 
-		ctx.String(200, "<h1>都道府県： "+prefectures+"</h1><h1>累計感染者数："+cumulative_value[0].Npatients+"</h1><h1>前日比："+"</h1>")
+		ctx.String(200, "<h1>都道府県： "+prefectures+"</h1><h1>累計感染者数："+cumulative_value[0].Npatients+"</h1><h1>前日比増加数："+strconv.Itoa(difference_1days)+"</h1><h1>1週間前比増加数："+strconv.Itoa(difference_7days)+"</h1>")
 	})
 
 	// サーバーを起動
@@ -73,4 +78,18 @@ func fetchApi(number_date string, encoded_prefecture string) []Item {
 	json.Unmarshal(body, &Item)
 
 	return Item.Value
+}
+
+func day_back(back_count int, string_date string) string {
+	t, _ := time.Parse("2006-01-02", string_date)
+	t2 := t.AddDate(0, 0, back_count)
+	t3 := t2.Format("20060102")
+	return t3
+}
+
+func subtract_strings(before string, after string) int {
+	before_int, _ := strconv.Atoi(before)
+	after_int, _ := strconv.Atoi(after)
+	difference := before_int - after_int
+	return difference
 }
